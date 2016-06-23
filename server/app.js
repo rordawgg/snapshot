c20a1679716b9571ef2c980dd6da99e13f4cd789
Max Pennington <your_email@example.com>
2016-06-23 13:23:17 -0700

    Set app to serve from static

--- server/app.js
+++ server/app.js
@@ -5,9 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -20,10 +17,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../client')));
-
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', express.static(path.join(__dirname, '../client')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
