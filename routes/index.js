267821e4b2227192c925b39cfe10410329802069
rordawgg <rory.dwy@gmail.com>
2016-06-23 08:39:03 -0700

    Add boiler plate routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  	res.render('index');
+});
+
+/* GET projects page */
+router.get('/projects', function(req, res, next) {
+	res.render('#project')
+});
+
+/* GET specific category */
+router.get('/projects/:name', function(req, res, next) {
+	res.render('#category.req.params.name')
 });
 
 module.exports = router;
