8761668962ed18156a38188544613277085f8ed3
Max Pennington <your_email@example.com>
2016-06-22 22:44:49 -0700

    Set up babel-node

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var express = require('express');
+// var express = require('express');
+import express from "express";
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
