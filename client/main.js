a04915572217e1f42cdb764109c0336720ec8f5b
Max Pennington <your_email@example.com>
2016-06-28 15:44:50 -0700

    Add vue resource

--- client/main.js
+++ client/main.js
@@ -1,11 +1,13 @@
 var Vue = require("vue");
 var Router = require("vue-router");
+var Http = require("vue-resource");
 
 var App = require("./components/views/app.vue");
 var Home = require("./components/views/home.vue");
 var Project = require("./components/views/project.vue");
 
 Vue.use(Router);
+Vue.use(Http);
 
 var router = new Router({history: true});
 
