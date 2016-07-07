74d1bf369196fad778ad21209ab56e21730856f5
rordawgg <rory.dwy@gmail.com>
2016-07-07 08:41:24 -0700

    create project list child component

---
+++ client/components/ProjectItem.vue
@@ -0,0 +1,29 @@
+<style lang='scss' scoped>
+	.project-item{
+		background: #fff url('http://lorempixel.com/400/200') no-repeat center center;
+		.project-name{
+			opacity: 0;
+			text-align: center;
+		}
+		&:hover{
+			.project-name{
+				opacity: 1;
+			}
+		}
+	}
+</style>
+
+<template>
+	<div class="project-item">
+		<div class="project-name">
+			<h1>Project Name</h1>
+		</div>
+		
+	</div>
+</template>
+
+<script>
+	module.exports = {
+		name: 'ProjectItem'
+	}
+</script>
