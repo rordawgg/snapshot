69c5076760bbb9ac671f7c1ba8bc6041977829ca
rordawgg <rory.dwy@gmail.com>
2016-07-07 09:04:18 -0700

    reformat project item

--- client/components/ProjectItem.vue
+++ client/components/ProjectItem.vue
@@ -1,7 +1,21 @@
 <style lang='scss' scoped>
+	@import '../styles/_main';
 	.project-item{
 		background: #fff url('http://lorempixel.com/400/200') no-repeat center center;
+		background-size: cover;
+		box-sizing: border-box;
+		height: 219px;
+
+		@include make-col();
+		@include make-col-span(12);
+
+		@include media-breakpoint-between(sm,xl) {
+			@include make-col-span(6);	
+		}
+
 		.project-name{
+			margin: auto 0 auto 0;
+			color: $primary;
 			opacity: 0;
 			text-align: center;
 		}
@@ -9,6 +23,18 @@
 			.project-name{
 				opacity: 1;
 			}
+		}
+
+		@include media-breakpoint-up(md) {
+			height: 260px;
+		}
+
+		@include media-breakpoint-up(lg) {
+			height: 318px;
+		}
+
+		@include media-breakpoint-up(xl) {
+			height: 431px;
 		}
 	}
 </style>
